635c543eeb83ec24392637fd99fdc9aa78852c27
thelma <thelmaguerrag@gmail.com>
2023-06-16 18:13:35 -0300

    add the copies of the exercises I had made from the course

---
+++ CS61B_WEEK_1/src/starTriangle.java
@@ -0,0 +1,13 @@
+public class starTriangle {
+    public static void result() {
+        String asterisk = "";
+
+        for(int i = 0; i < 5; i++) {
+            asterisk = asterisk + "*";
+            System.out.println(asterisk);
+        }
+    }
+    public static void main(String[] args) {
+        result();
+    }
+}
