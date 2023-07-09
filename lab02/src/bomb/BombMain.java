cf89d55d74ad14260ad923623fee2232ed833622
Thelma <thelmaguerrag@gmail.com>
2023-07-09 11:53:08 -0300

    Completed lab02 (adventure beeCountingState and bomb)

--- lab02/src/bomb/BombMain.java
+++ lab02/src/bomb/BombMain.java
@@ -10,14 +10,25 @@ public class BombMain {
         }
         // TODO: Find the correct passwords to each phase using debugging techniques
         Bomb b = new Bomb();
+
+        StringBuilder password2 = new StringBuilder();
+
+        for(int i = 0; i < 1338; i++) {
+            if(i == 1337) {
+                password2.append(Integer.toString(-81201430)).append(" ");
+                continue;
+            }
+            password2.append(Integer.toString(i)).append(" ");
+        }
+
         if (phase >= 0) {
-            b.phase0("Figure this out. I wonder where the phases are defined...");
+            b.phase0("39291226");
         }
         if (phase >= 1) {
-            b.phase1(null); // Figure this out too
+            b.phase1(IntList.of(0,9,3,0,8)); // Figure this out too
         }
         if (phase >= 2) {
-            b.phase2("Figure this out. I wonder where the phases are defined...");
+            b.phase2(password2.toString());
         }
     }
 }
